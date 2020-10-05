d70c75e9005859e401d32e3897a454ba68c7d369
Joury02x <70814221+Joury02x@users.noreply.github.com>
2020-10-05 17:02:00 +0300

    done

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -7,19 +7,19 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+let name2 = "Salem"
 
 
 var myWeight = 60
-var FriendWeight = 80.5
-var height = 1.5
+var FriendWeight: Double = 80.5
+var height: Double = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = Double(myWeight) + FriendWeight
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = Double(myWeight) * (height * height)
 
 /*
 - - - -
@@ -38,30 +38,41 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Bashayer"
+var lastName1 = "Alotaibi"
+var email1 = "26Bushayer@gmail.com"
+var phoneNumber1 = "94789101"
+var age1 = 8
+var country1 = "Kuwait"
+var area1 = "Adan"
+var password = "1234"
+var isKuwaiti = true
+
 
 //Person 2
 
-
-
-
-
-
-
-
+var firstName2 = "Areej"
+var lastName2 = "Saif"
+var email2 = "Areej@gmail.com"
+var phoneNumber2 = "98034321"
+var age2 = 16
+var country2 = "Kuwait"
+var area2 = "Adan"
+var password2 = "5678"
+var isKuwaiti2 = true
 
 
 //Person 3
 
-
+var firstName3 = "Jay"
+var lastName3 = "Alotaibi"
+var email3 = "26Joury@gmail.com"
+var phoneNumber3 = "99446901"
+var age3 = 18
+var country3 = "Iceland"
+var area3 = "Ice"
+var password3 = "9012"
+var isKuwaiti3 = false
 
 
 
@@ -82,3 +93,13 @@ var isKuwaiti = false
 
  
  */
+
+var mutualAge = age1 == age2 || age2 == age3 || age3 == age1
+var mutualInfo = country1 == country2 || country1 == country3 || country2 == country3
+var adult = age1 > 18 || age2 > 18 || age3 > 18
+var underage = age1 < 18 || age2 < 18 || age3 < 18
+var age17 = age1 == 17 || age2 == 17 || age3 == 17
+var underAge17 = age1 != 17 || age2 != 17 || age3 != 17
+
+let string = "iOS"
+print(string)
